70efaaffa2e691e02a5a9f1fa4378a02a032c214
Smartoneinok <Alexander_Novotny@aol.com>
2023-04-18 11:13:51 -0500

    adding copy

--- api/webhook.js
+++ api/webhook.js
@@ -1,21 +1,29 @@
 const Box = require('box-node-sdk');
 
-export default async function boxskill(request, response) {
+export default async function webhook(request, response) {
 
-    const sdkConfig = {
-        boxAppSettings: {
-            clientID: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET
-        }, 
-        enterpriseID: "273262935" 
+    let isValid = Box.validateWebhookMessage(request.body, request.headers, process.env.primaryKey, process.env.secondaryKey);
+
+    if (isValid) {
+      const sdkConfig = {
+          boxAppSettings: {
+              clientID: process.env.CLIENT_ID,
+              clientSecret: process.env.CLIENT_SECRET
+          }, 
+          enterpriseID: "273262935" 
+      }
+      const sdk = Box.getPreconfiguredInstance(sdkConfig)
+
+      const client = sdk.getAnonymousClient(); 
+      
+      await client.files.copy('1190810873364',request.body.source.id)
+      
+      response.status(200).json({
+        info: 'success'
+      });
+    } else {
+      response.status(200).json({
+        info: 'Error'
+      });
     }
-    const sdk = Box.getPreconfiguredInstance(sdkConfig)
-
-    const client = sdk.getAnonymousClient(); 
-
-    let fileInfo = await client.files.get("1190810873364");
-
-    response.status(200).json({
-      info: JSON.stringify(fileInfo)
-    });
   }
