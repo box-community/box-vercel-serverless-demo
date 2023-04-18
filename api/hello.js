79e0646ec903bd258deefe1fde8327d31e0be547
Smartoneinok <Alexander_Novotny@aol.com>
2023-04-18 10:31:08 -0500

    changing routes

--- api/hello.js
+++
@@ -1,8 +0,0 @@
-export default function hello(request, response) {
-    response.status(200).json({
-      body: request.body,
-      query: request.query,
-      cookies: request.cookies,
-      env: process.env.MY_SECRET,
-    });
-  }
