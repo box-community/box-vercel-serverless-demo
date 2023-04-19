b3317c56b8b503b27f16e57bacbdeeda9dddb038
Smartoneinok <Alexander_Novotny@aol.com>
2023-04-19 15:55:32 -0500

    comments

--- api/webhook.js
+++ api/webhook.js
@@ -2,6 +2,7 @@ const Box = require('box-node-sdk');
 
 export default async function webhook(request, response) {
 
+    //This line will validate that box is the service calling the webhook. 
     let isValid = Box.validateWebhookMessage(request.body, request.headers, process.env.primaryKey, process.env.secondaryKey);
 
     if (isValid) {
@@ -21,9 +22,10 @@ export default async function webhook(request, response) {
           id: request.body.source.id,
           type: request.body.source.type
         },
+        //You can edit this field (or remove it)
         title: "Case Document Request"
       })
-      
+    //If the service fails for some reason, still return a 200. This keeps Box from calling again and again.    
       response.status(200).json({
         info: 'success'
       });
